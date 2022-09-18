21d18295f041aef55373413e60de8f5d9643ddcb
jthebat <taein007@gmail.com>
2022-09-18 12:08:48 +0900

    [TaeIn]<feat> access&refresh tokens for login process

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,8 +1,9 @@
-import { NextFunction, Request, Response } from 'express';
+import e, { NextFunction, Request, Response } from 'express';
 // import { Connect, Query } from '../config/mysql';
 import logging from '../config/logging';
 import passport from 'passport';
-
+import connectDB from '../config/mysql';
+import jwt from 'jsonwebtoken';
 import config from '../config/config';
 
 const NAMESPACE = 'Users';
@@ -87,16 +88,59 @@ const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
 */
 //카카오 콜백
 const kakaoCallback = async (req: Request, res: Response, next: NextFunction) => {
-    passport.authenticate(
-        'kakao',
-        { failureRedirect: '/' },
-        (err, user, info) => {
-            if (err) return next(err);
+    passport.authenticate('kakao', { failureRedirect: '/' }, (err, user, info) => {
+        if (err) return next(err);
+        /**refreshtoken 생성 */
+        const refreshToken = jwt.sign({}, config.jwt.secretKey as jwt.Secret, {
+            expiresIn: '14d',
+            issuer: 'Martian'
+        });
+        /**accesstoken 생성 서비스 특성상 시간을 길게 보장해줘야하는지 아니면 refreshtoken이 해결해줄수 있는 부분인지? ?? */
+        const accessToken = jwt.sign({ info }, config.jwt.secretKey as jwt.Secret, {
+            /**example of expiresIn */
+            /*
+            ms('2 days')  // 172800000
+            ms('1d')      // 86400000
+            ms('10h')     // 36000000
+            ms('2.5 hrs') // 9000000
+            ms('2h')      // 7200000
+            ms('1m')      // 60000
+            ms('5s')      // 5000
+            ms('1y')      // 31557600000
+            ms('100')     // 100
+            ms('-3 days') // -259200000
+            ms('-1h')     // -3600000
+            ms('-200')    // -200
+            */
+            expiresIn: '5m', //for test
+            // expiresIn: '1h',
+            issuer: 'Martian'
+        });
+        /**queries */
+        const updateQuery = `update users set refreshtoken = ? where snsId = ?`;
+        const insertQuery = `INSERT INTO users (snsId, email, nickname, provider, refreshtoken) VALUE (?,?,?,?,?)`;
 
-        res.status(200).json({
-            status: 'success'});
-
+        if (user.length === 0) {
+            // 해당되는 user가 없으면 DB에 넣기
+            connectDB.query(insertQuery, [info.snsId, info.email, info.nickname, info.provider, refreshToken], function (error, result) {
+                if (error) return console.log(error);
+                else {
+                    /**front와 연결후 redirect 주소로 연결 필요  */
+                    res.status(200).cookie('refreshToken', refreshToken).cookie('accessToken', accessToken).json({ status: 'success' });
+                    //res.status(200).cookie('refreshToken', refreshToken).cookie('accessToken', accessToken).redirect(`http://www.naver.com`);
+                }
+            });
+        } else {
+            connectDB.query(updateQuery, [refreshToken, info.snsId], function (error, result) {
+                if (error) return console.log(error);
+                else {
+                    /**front와 연결후 redirect 주소로 연결 필요 */
+                    res.status(200).cookie('refreshToken', refreshToken).cookie('accessToken', accessToken).json({ status: 'success' });
+                    //res.status(200).cookie('refreshToken', refreshToken).cookie('accessToken', accessToken).redirect(`http://www.naver.com`);
+                }
+            });
+        }
     })(req, res, next);
 };
 
-export default {  kakaoCallback };
+export default { kakaoCallback, userInfo };
